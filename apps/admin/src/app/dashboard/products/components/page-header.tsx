d9df19b7127220b26ce190974fbc59e5b0e47818
code99-ash <oyetikh1@gmail.com>
2024-11-14 07:46:01 +0000

    cleanups

--- apps/admin/src/app/dashboard/products/components/page-header.tsx
+++ apps/admin/src/app/dashboard/products/components/page-header.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from 'react'
 import { Button } from '@/components/ui/button';
-import { SelectedContext } from './product-list';
 import AssignToManyProducts from './products/many/assign-many';
 
 
